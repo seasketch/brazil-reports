ab41fe5aa6b57560a9a1b2d1f565d8a34f592798
petermenzies <petermenzies@ucsb.edu>
2023-07-10 16:12:51 -0700

    add miningOverlap function

---
+++ src/functions/miningOverlapSmoke.test.ts
@@ -0,0 +1,23 @@
+/**
+ * @jest-environment node
+ * @group smoke
+ */
+import { miningOverlap } from "./miningOverlap";
+import {
+  getExamplePolygonSketchAll,
+  writeResultOutput,
+} from "@seasketch/geoprocessing/scripts/testing";
+
+describe("Basic smoke tests", () => {
+  test("handler function is present", () => {
+    expect(typeof miningOverlap).toBe("function");
+  });
+  test("miningOverlapSmoke - tests run against all examples", async () => {
+    const examples = await getExamplePolygonSketchAll();
+    for (const example of examples) {
+      const result = await miningOverlap(example);
+      expect(result).toBeTruthy();
+      writeResultOutput(result, "miningOverlap", example.properties.name);
+    }
+  }, 120000);
+});
