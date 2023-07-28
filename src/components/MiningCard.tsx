bcfb9fc527b9da1c2303319397d87aebd6e299db
petermenzies <petermenzies@ucsb.edu>
2023-07-28 11:14:31 -0700

    add region selection to mining overlap and existing MPAs to example sketches

--- src/components/MiningCard.tsx
+++ src/components/MiningCard.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   Collapse,
   ClassTable,
@@ -16,6 +16,7 @@ import {
   toPercentMetric,
   squareMeterToKilometer,
   valueFormatter,
+  sortMetrics,
 } from "@seasketch/geoprocessing/client-core";
 
 import project from "../../project";
@@ -29,6 +30,14 @@ const precalcMetrics = project.getPrecalcMetrics(
   metricGroup.classKey
 );
 
+// Mapping region ids to display names for report
+const regions: { [id: string]: string } = {};
+regions["all"] = "All";
+regions["north"] = "North";
+regions["south"] = "South";
+regions["northeast"] = "Northeast";
+regions["central-east"] = "Central-East";
+
 const Number = new Intl.NumberFormat("en", { style: "decimal" });
 
 export const MiningCard = () => {
@@ -41,19 +50,25 @@ export const MiningCard = () => {
   const percAreaWithin = `% ${t("Area Within Plan")}`;
   const sqKmLabel = t("km²");
 
+  const [region, setRegionVisible] = useState("all");
+
+  const regionSwitcher = (e: any) => {
+    setRegionVisible(e.target.value);
+  };
+
   return (
     <>
       <ResultsCard
         title={t("Mining Areas")}
         functionName="miningOverlap"
-        useChildCard
+        // useChildCard
       >
         {(data: ReportResult) => {
-          let singleMetrics = data.metrics.filter(
+          const singleMetrics = data.metrics.filter(
             (m) => m.sketchId === data.sketch.properties.id
           );
 
-          const finalMetrics = [
+          const parentMetrics = [
             ...singleMetrics,
             ...toPercentMetric(
               singleMetrics,
@@ -62,123 +77,92 @@ export const MiningCard = () => {
             ),
           ];
 
-          return (
-            <ToolbarCard
-              title={t("Mining Areas")}
-              items={
-                <LayerToggle
-                  label={mapLabel}
-                  layerId={metricGroup.layerId}
-                  simple
-                />
+          console.log(parentMetrics);
+
+          // grouping metrics by region suffix
+          const groupedMetrics = parentMetrics.reduce<Record<string, any>>(
+            (groups, metric) => {
+              // get region id from classId suffix (i.e. "south", "central-east")
+              const region: string | undefined = metric.classId
+                ?.replace("mining_potential_", "")
+                .replace("mining_active_", "");
+
+              if (!region) {
+                console.log(metric.classId);
+                return groups;
               }
-            >
-              <Translator>
-                <ClassTable
-                  rows={finalMetrics}
-                  metricGroup={metricGroup}
-                  columnConfig={[
-                    {
-                      columnLabel: typeLabel,
-                      type: "class",
-                      width: 30,
-                    },
-                    {
-                      columnLabel: areaWithin,
-                      type: "metricValue",
-                      metricId: metricGroup.metricId,
-                      valueFormatter: (val: string | number) =>
-                        Number.format(
-                          Math.round(
-                            squareMeterToKilometer(
-                              typeof val === "string" ? parseInt(val) : val
-                            )
+              // adds metric to the region's metric array
+              groups[region] = [...(groups[region] || []), metric];
+              return groups;
+            },
+            {}
+          );
+
+          return (
+            <>
+              <p>
+                {t("Mining overlap with region:  ")}
+                <select onChange={regionSwitcher}>
+                  {Object.keys(regions).map((region: string) => {
+                    return (
+                      <option key={region} value={region}>
+                        {regions[region]}
+                      </option>
+                    );
+                  })}
+                </select>
+              </p>
+              <ClassTable
+                rows={groupedMetrics[region]}
+                metricGroup={metricGroup}
+                columnConfig={[
+                  {
+                    columnLabel: typeLabel,
+                    type: "class",
+                    width: 30,
+                  },
+                  {
+                    columnLabel: areaWithin,
+                    type: "metricValue",
+                    metricId: metricGroup.metricId,
+                    valueFormatter: (val: string | number) =>
+                      Number.format(
+                        Math.round(
+                          squareMeterToKilometer(
+                            typeof val === "string" ? parseInt(val) : val
                           )
-                        ),
-                      valueLabel: sqKmLabel,
-                      width: 30,
+                        )
+                      ),
+                    valueLabel: sqKmLabel,
+                    width: 30,
+                  },
+                  {
+                    columnLabel: percAreaWithin,
+                    type: "metricChart",
+                    metricId: project.getMetricGroupPercId(metricGroup),
+                    valueFormatter: "percent",
+                    chartOptions: {
+                      showTitle: true,
                     },
-                    {
-                      columnLabel: percAreaWithin,
-                      type: "metricChart",
-                      metricId: project.getMetricGroupPercId(metricGroup),
-                      valueFormatter: "percent",
-                      chartOptions: {
-                        showTitle: true,
-                        targetLabelPosition: "bottom",
-                        targetLabelStyle: "tight",
-                        barHeight: 11,
-                      },
-                      width: 30,
-                      targetValueFormatter: (
-                        value: number,
-                        row: number,
-                        numRows: number
-                      ) => {
-                        if (row === 0) {
-                          return (value: number) =>
-                            `${valueFormatter(value / 100, "percent0dig")} ${t(
-                              "Target"
-                            )}`;
-                        } else {
-                          return (value: number) =>
-                            `${valueFormatter(value / 100, "percent0dig")}`;
-                        }
-                      },
-                    },
-                    {
-                      columnLabel: mapLabel,
-                      type: "layerToggle",
-                      width: 10,
-                    },
-                  ]}
-                />
-              </Translator>
-
-              {isCollection && (
-                <Collapse title={t("Show by MPA")}>
-                  {genSketchTable(data)}
-                </Collapse>
-              )}
-
+                    width: 45,
+                  },
+                  {
+                    columnLabel: mapLabel,
+                    type: "layerToggle",
+                    width: 10,
+                  },
+                ]}
+              />
               <Collapse title={t("Learn more")}>
-                <Trans i18nKey="Priority Areas Card - learn more">
-                  <p>
-                    {" "}
-                    This report summarizes the proportion of Active and
-                    potential mining areas within this plan.
-                  </p>
-                  <p>
-                    If MPA boundaries overlap with each other, the overlap is
-                    only counted once.
-                  </p>
-                </Trans>
+                <p>
+                  This report summarizes overlap with active and potential
+                  mining areas by region.
+                </p>
               </Collapse>
-            </ToolbarCard>
+            </>
           );
         }}
       </ResultsCard>
     </>
-  );
-};
-
-const genSketchTable = (data: ReportResult) => {
-  // Build agg metric objects for each child sketch in collection with percValue for each class
-  const childSketches = toNullSketchArray(data.sketch);
-  const childSketchIds = childSketches.map((sk) => sk.properties.id);
-  const childSketchMetrics = toPercentMetric(
-    metricsWithSketchId(
-      data.metrics.filter((m) => m.metricId === metricGroup.metricId),
-      childSketchIds
-    ),
-    precalcMetrics
-  );
-  const sketchRows = flattenBySketchAllClass(
-    childSketchMetrics,
-    metricGroup.classes,
-    childSketches
-  );
-  return (
-    <SketchClassTable rows={sketchRows} metricGroup={metricGroup} formatPerc />
   );
 };
