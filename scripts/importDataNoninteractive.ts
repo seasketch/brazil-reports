24301d416b8f1028c733f12414bf6f7ef2f7c9ad
petermenzies <petermenzies@ucsb.edu>
2023-08-23 17:49:46 +0000

    add script to update data from db and config debug

--- scripts/importDataNoninteractive.ts
+++ scripts/importDataNoninteractive.ts
@@ -33,21 +33,111 @@ function vectorMapper(
 const vectorMapperInputs: ImportVectorDatasourceAnswers[] = [
   {
     geo_type: "vector",
-    src: "/workspaces/brazil-reports/data/src/fromDatabase/human.mining_active_anm.json",
-    layerName: "",
+    src: "data/src/fromDatabase/usos_humanos.eolico_parques.json",
+    layerName: "eolico_parques",
     classKeys: "",
     propertiesToKeep: "",
     formats: ["fgb"],
-    datasourceId: "mining_active_test"
+    datasourceId: "wind_farms"
   },
   {
     geo_type: "vector",
-    src: "/workspaces/brazil-reports/data/src/fromDatabase/human.mining_potential_dnpm.json",
-    layerName: "",
+    src: "data/src/fromDatabase/usos_humanos.eolico_torres.json",
+    layerName: "eolico_torres",
     classKeys: "",
     propertiesToKeep: "",
     formats: ["fgb"],
-    datasourceId: "mining_potential_test"
+    datasourceId: "wind_towers"
+  },
+  {
+    geo_type: "vector",
+    src: "data/src/fromDatabase/usos_humanos.eolico_linhas_transmissao.json",
+    layerName: "eolico_linhas_transmissao",
+    classKeys: "",
+    propertiesToKeep: "",
+    formats: ["fgb"],
+    datasourceId: "power_lines"
+  },
+  {
+    geo_type: "vector",
+    src: "data/src/fromDatabase/public.batimetria_classes.json",
+    layerName: "batimetria_classes",
+    classKeys: "class",
+    propertiesToKeep: "",
+    formats: ["fgb"],
+    datasourceId: "bathymetric_classes"
+  },
+  {
+    geo_type: "vector",
+    src: "data/src/fromDatabase/usos_humanos.mineracao_areas_potenciais.json",
+    layerName: "mineracao_areas_potenciais",
+    classKeys: "",
+    propertiesToKeep: "",
+    formats: ["fgb"],
+    datasourceId: "mining_potential"
+  },
+  {
+    geo_type: "vector",
+    src: "data/src/fromDatabase/usos_humanos.mineracao_processos.json",
+    layerName: "mineracao_processos",
+    classKeys: "",
+    propertiesToKeep: "",
+    formats: ["fgb"],
+    datasourceId: "mining_process"
+  },
+  {
+    geo_type: "vector",
+    src: "data/src/fromDatabase/usos_humanos.petroleo_blocos_concessao.json",
+    layerName: "petroleo_blocos_concessao",
+    classKeys: "",
+    propertiesToKeep: "",
+    formats: ["fgb"],
+    datasourceId: "oil_blocks_concession"
+  },
+  {
+    geo_type: "vector",
+    src: "data/src/fromDatabase/usos_humanos.petroleo_campos_producao.json",
+    layerName: "petroleo_campos_producao",
+    classKeys: "",
+    propertiesToKeep: "",
+    formats: ["fgb"],
+    datasourceId: "oil_production_fields"
+  },
+  {
+    geo_type: "vector",
+    src: "data/src/fromDatabase/usos_humanos.petroleo_campos_producao.json",
+    layerName: "petroleo_campos_producao",
+    classKeys: "",
+    propertiesToKeep: "",
+    formats: ["fgb"],
+    datasourceId: "oil_production_fields"
+  },
+  {
+    geo_type: "vector",
+    src: "data/src/fromDatabase/usos_humanos.petroleo_plataformas.json",
+    layerName: "petroleo_plataformas",
+    classKeys: "",
+    propertiesToKeep: "",
+    formats: ["fgb"],
+    datasourceId: "oil_platforms"
+  },
+  {
+    geo_type: "vector",
+    src: "data/src/fromDatabase/usos_humanos.portos.json",
+    layerName: "portos",
+    classKeys: "",
+    propertiesToKeep: "",
+    formats: ["fgb"],
+    datasourceId: "ports"
+  },
+  {
+    geo_type: "vector",
+    src: "data/src/fromDatabase/usos_humanos.portos.json",
+    layerName: "portos",
+    classKeys: "",
+    propertiesToKeep: "",
+    formats: ["fgb"],
+    datasourceId: "ports"
   }
 ]
 
