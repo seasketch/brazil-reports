6324dfc65489c3fea4842d4587e0d62820a2d408
Tim Welch <tim.j.welch@gmail.com>
2024-06-25 17:12:01 -0700

    run upgrade, extract translations

--- project/projectClient.ts
+++ project/projectClient.ts
@@ -3,7 +3,7 @@ import metrics from "./metrics.json";
 import objectives from "./objectives.json";
 import basic from "./basic.json";
 import projectPackage from "../package.json";
-import gp from "../geoprocessing.json";
+import gp from "./geoprocessing.json";
 
 import { ProjectClientBase } from "@seasketch/geoprocessing";
 
