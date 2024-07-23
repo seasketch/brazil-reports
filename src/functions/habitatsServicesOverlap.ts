aa37025636185c3c29c6e7f3dfce62fade2741ce
petermenzies <petermenzies@ucsb.edu>
2024-07-23 23:04:25 +0000

    use workers for habitats overlap

--- src/functions/habitatsServicesOverlap.ts
+++ src/functions/habitatsServicesOverlap.ts
@@ -1,108 +1,100 @@
 import {
   Sketch,
-  Feature,
-  GeoprocessingHandler,
-  Metric,
-  Polygon,
-  ReportResult,
   SketchCollection,
-  toNullSketch,
-  overlapFeatures,
-  rekeyMetrics,
-  getFlatGeobufFilename,
-  isInternalVectorDatasource,
+  Polygon,
+  MultiPolygon,
+  GeoprocessingHandler,
+  DefaultExtraParams,
 } from "@seasketch/geoprocessing";
-import { fgbFetchAll } from "@seasketch/geoprocessing/dataproviders";
-import bbox from "@turf/bbox";
-import truncate from "@turf/truncate";
 import project from "../../project/projectClient.js";
+import {
+  GeoprocessingRequestModel,
+  Metric,
+  ReportResult,
+  rekeyMetrics,
+  sortMetrics,
+  toNullSketch,
+} from "@seasketch/geoprocessing/client-core";
+import { parseLambdaResponse, runLambdaWorker } from "../util/lambdaHelpers.js";
+import awsSdk from "aws-sdk";
+import { habitatsServicesOverlapWorker } from "./habitatsServicesOverlapWorker.js";
 
+/**
+ * habitatsServicesOverlap: A geoprocessing function that calculates overlap metrics
+ * @param sketch - A sketch or collection of sketches
+ * @param extraParams
+ * @returns Calculated metrics and a null sketch
+ */
 export async function habitatsServicesOverlap(
-  sketch: Sketch<Polygon> | SketchCollection<Polygon>
+  sketch:
+    | Sketch<Polygon | MultiPolygon>
+    | SketchCollection<Polygon | MultiPolygon>,
+  extraParams: DefaultExtraParams = {},
+  request?: GeoprocessingRequestModel<Polygon | MultiPolygon>
 ): Promise<ReportResult> {
-  const box = sketch.bbox || bbox(sketch);
   const metricGroup = project.getMetricGroup("habitatsServicesOverlap");
+  const geographies = project.geographies;
 
-  let cachedFeatures: Record<string, Feature<Polygon>[]> = {};
+  try {
+    const allMetrics = await Promise.all(
+      geographies.map(async (geography) => {
+        const classMetrics = await Promise.all(
+          metricGroup.classes.map(async (curClass) => {
+            const parameters = {
+              ...extraParams,
+              geography: geography,
+              metricGroup,
+              classId: curClass.classId,
+            };
 
-  const polysByBoundary = (
-    await Promise.all(
-      metricGroup.classes.map(async (curClass) => {
-        if (!curClass.datasourceId) {
-          throw new Error(`Missing datasourceId ${curClass.classId}`);
-        }
-        const ds = project.getDatasourceById(curClass.datasourceId);
-        if (isInternalVectorDatasource(ds)) {
-          const url = `${project.dataBucketUrl()}${getFlatGeobufFilename(ds)}`;
+            console.log(
+              `Processing classId: ${curClass.classId} for geography: ${geography}`
+            );
 
-          // Fetch features overlapping with sketch, pull from cache if already fetched
-          const dsFeatures =
-            cachedFeatures[curClass.datasourceId] ||
-            (await fgbFetchAll<Feature<Polygon>>(url, box));
-          cachedFeatures[curClass.datasourceId] = dsFeatures;
-
-          // If this is a sub-class, filter by class name, exclude null geometry too
-          // ToDo: should do deeper match to classKey
-          const finalFeatures =
-            ds.classKeys.length > 0 &&
-            curClass.classId !== `${ds.datasourceId}_all`
-              ? dsFeatures.filter((feat) => {
-                  return (
-                    feat.geometry &&
-                    feat.properties![ds.classKeys[0]] === curClass.classId
-                  );
-                }, [])
-              : dsFeatures;
-
-          // truncate vertex precision to 6 decimal places - without this turf throws an error when overlapFeatures() is called
-          const finalFeaturesTrunc = finalFeatures.map((feat) => {
-            const trunc = truncate(feat);
-            return trunc;
-          });
-          return finalFeaturesTrunc;
-        }
-        return [];
-      })
-    )
-  ).reduce<Record<string, Feature<Polygon>[]>>((acc, polys, classIndex) => {
-    return {
-      ...acc,
-      [metricGroup.classes[classIndex].classId]: polys,
-    };
-  }, {});
-
-  const metrics: Metric[] = (
-    await Promise.all(
-      metricGroup.classes.map(async (curClass) => {
-        const overlapResult = await overlapFeatures(
-          metricGroup.metricId,
-          polysByBoundary[curClass.classId],
-          sketch
-        );
-        return overlapResult.map(
-          (metric): Metric => ({
-            ...metric,
-            classId: curClass.classId,
+            return process.env.NODE_ENV === "test"
+              ? habitatsServicesOverlapWorker(sketch, parameters)
+              : runLambdaWorker(
+                  sketch,
+                  parameters,
+                  "habitatsServicesOverlapWorker",
+                  request
+                );
           })
         );
-      })
-    )
-  ).reduce(
-    // merge
-    (metricsSoFar, curClassMetrics) => [...metricsSoFar, ...curClassMetrics],
-    []
-  );
 
-  return {
-    metrics: rekeyMetrics(metrics),
-    sketch: toNullSketch(sketch, true),
-  };
+        console.log(
+          `Results for geography ${geography.geographyId}:`,
+          classMetrics
+        );
+
+        return classMetrics.flat();
+      })
+    );
+
+    const metrics = allMetrics
+      .flat()
+      .reduce<
+        Metric[]
+      >((acc, lambdaResult) => acc.concat(process.env.NODE_ENV === "test" ? (lambdaResult as Metric[]) : parseLambdaResponse(lambdaResult as awsSdk.Lambda.InvocationResponse)), []);
+
+    console.log("Final metrics:", metrics);
+
+    return {
+      metrics: sortMetrics(rekeyMetrics(metrics)),
+      sketch: toNullSketch(sketch, true),
+    };
+  } catch (error) {
+    console.error("Error fetching metrics:", error);
+    throw error;
+  }
 }
 
 export default new GeoprocessingHandler(habitatsServicesOverlap, {
   title: "habitatsServicesOverlap",
-  description: "Calculate sketch overlap with bathymetric classes",
+  description: "habitatsServicesOverlap overlap",
+  timeout: 500, // seconds
+  memory: 1024, // megabytes
   executionMode: "async",
-  timeout: 600,
+  // Specify any Sketch Class form attributes that are required
   requiresProperties: [],
 });
